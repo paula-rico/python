1fd6f7dd66b51c22b4569f139b68e538d0c59a7f
paula-rico <86172702+paula-rico@users.noreply.github.com>
2022-12-24 19:02:49 -0300

    Delete MVT directory

--- MVT/Familia/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FamiliaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Familia'
--- MVT/Familia/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class familia (models.Model):
-    name = models.CharField(max_length=100)
-    age = models.FloatField()
-    married = models.BooleanField()
-    birth = models.DateField()
